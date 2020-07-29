21c3be83a714155eeae1666f3c29cf82c9e15490
Torben Neufeldt <torbenneufeldt@gmail.com>
2020-07-29 14:45:42 +0200

    Add check e2e test

---
+++ e2e.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+image="tenjaa/concourse-github-app-token:${GITHUB_SHA}"
+
+docker pull "${image}"
+
+checkEmpty=$(echo '{}' | docker run "${image}" /opt/resource/check)
+checkReturnGiven=$(echo '{"version":{"date":"some-date"}}' | docker run "${image}" /opt/resource/check)
+
+if [ "$checkEmpty" != "[]" ]; then
+  echo "Invalid checkEmpty: ${checkEmpty}"
+  exit 1
+fi
+
+if [ "$checkReturnGiven" != '[{"date":"some-date"}]' ]; then
+  echo "Invalid checkEmpty: ${checkReturnGiven}"
+  exit 1
+fi
