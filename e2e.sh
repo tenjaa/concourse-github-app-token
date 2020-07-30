87e31f5ceb3b77b6017e8de6df50261e18ebea05
Torben Neufeldt <torbenneufeldt@gmail.com>
2020-07-30 12:03:33 +0200

    Add out e2e test

--- e2e.sh
+++ e2e.sh
@@ -4,7 +4,7 @@ image="tenjaa/concourse-github-app-token:${GITHUB_SHA}"
 
 docker pull "${image}"
 
-checkEmpty=$(echo '{}' | docker run -i "${image}" /opt/resource/check | jq -c '.')
+checkEmpty=$(echo '{}' | docker run -i "${image}" /opt/resource/check)
 checkReturnGiven=$(echo "{\"version\":{\"date\":\"some-date\"}}" | docker run -i "${image}" /opt/resource/check)
 
 if [[ "$checkEmpty" != "[]" ]]; then
@@ -15,4 +15,10 @@ fi
 if [[ "$checkReturnGiven" != '[{"date":"some-date"}]' ]]; then
   echo "Invalid checkReturnGiven: ${checkReturnGiven}"
   exit 1
+fi
+
+outDate=$(echo 'stdin is ignored anyway' | docker run -i "${image}" /opt/resource/out | jq '.version.date')
+
+if ! date --iso-8601 -d "${outDate}"; then
+  echo "Error parsing date: outDate"
 fi
