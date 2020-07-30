1188e672e83a0f5eef1c8e3735ef756682f177d6
Torben Neufeldt <torbenneufeldt@gmail.com>
2020-07-30 10:21:17 +0200

    Add -i to docker run

--- e2e.sh
+++ e2e.sh
@@ -5,8 +5,8 @@ image="tenjaa/concourse-github-app-token:${GITHUB_SHA}"
 
 docker pull "${image}"
 
-checkEmpty=$(echo '{}' | docker run "${image}" /opt/resource/check | jq -c '.')
-checkReturnGiven=$(echo "{\"version\":{\"date\":\"some-date\"}}" | docker run "${image}" /opt/resource/check)
+checkEmpty=$(echo '{}' | docker run -i "${image}" /opt/resource/check | jq -c '.')
+checkReturnGiven=$(echo "{\"version\":{\"date\":\"some-date\"}}" | docker run -i "${image}" /opt/resource/check)
 
 if [[ "$checkEmpty" != "[]" ]]; then
   echo "Invalid checkEmpty: ${checkEmpty}"
