34425159af78ffc86b0674000049925d27f246ce
Torben Neufeldt <torbenneufeldt@gmail.com>
2020-07-30 10:04:32 +0200

    Fix checkReturnGiven e2e test

--- e2e.sh
+++ e2e.sh
@@ -6,7 +6,7 @@ image="tenjaa/concourse-github-app-token:${GITHUB_SHA}"
 docker pull "${image}"
 
 checkEmpty=$(echo '{}' | docker run "${image}" /opt/resource/check | jq -c '.')
-checkReturnGiven=$(echo '{"version":{"date":"some-date"}}' | docker run "${image}" /opt/resource/check)
+checkReturnGiven=$(echo "{\"version\":{\"date\":\"some-date\"}}" | docker run "${image}" /opt/resource/check)
 
 if [[ "$checkEmpty" != "[]" ]]; then
   echo "Invalid checkEmpty: ${checkEmpty}"
@@ -14,6 +14,6 @@ if [[ "$checkEmpty" != "[]" ]]; then
 fi
 
 if [[ "$checkReturnGiven" != '[{"date":"some-date"}]' ]]; then
-  echo "Invalid checkEmpty: ${checkReturnGiven}"
+  echo "Invalid checkReturnGiven: ${checkReturnGiven}"
   exit 1
 fi
