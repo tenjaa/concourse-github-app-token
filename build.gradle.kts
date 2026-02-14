332bcd71c237f6e29148cbe26063ebe0d58929b6
Torben Neufeldt <github@neufeldt.eu>
2026-02-14 14:55:00 +0100

    Adapt dependencies to latest updates from official guide

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,15 +8,13 @@ repositories {
 }
 
 dependencies {
-    val junitVersion = "6.0.2"
-
     implementation("commons-io:commons-io:2.20.0")
     implementation("com.auth0:java-jwt:4.3.0")
     implementation("com.google.code.gson:gson:2.13.2")
     implementation("org.json:json:20251224")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:${junitVersion}")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${junitVersion}")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:${junitVersion}")
+    testImplementation(platform("org.junit:junit-bom:6.0.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("org.assertj:assertj-core:3.27.7")
     testImplementation("com.squareup.okhttp3:mockwebserver:5.3.2")
     testImplementation("com.google.jimfs:jimfs:1.3.1")
